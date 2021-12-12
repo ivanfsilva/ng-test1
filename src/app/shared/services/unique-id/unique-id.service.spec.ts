05de17dba13e836baa8be93554b064d5e3bd2827
ivanfsilva <ivanfs27@gmail.com>
2021-12-12 12:43:57 -0300

    using beforeEach()

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,8 +1,14 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe( UniqueIdService.name, () => {
+
+  let service: UniqueIdService = null;
+
+  beforeEach( () => {
+    service = new UniqueIdService();
+  });
+
   it(`#${ UniqueIdService.prototype.generateUniqueIdWithPrefix.name } should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
 
     expect( id.startsWith( 'app-' ) ).toBeTrue();
@@ -10,7 +16,6 @@ describe( UniqueIdService.name, () => {
 
   it(`#${ UniqueIdService.prototype.generateUniqueIdWithPrefix.name } should not generate duplicate IDs when called multiple times `,
     () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for ( let i = 0; i < 50; i++ ) {
       ids.add( service.generateUniqueIdWithPrefix( 'app' ) );
@@ -20,7 +25,6 @@ describe( UniqueIdService.name, () => {
   });
 
   it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
-    const service = new UniqueIdService();
     service.generateUniqueIdWithPrefix( 'app' );
     service.generateUniqueIdWithPrefix( 'app' );
 
