7ff5d5912254c2dd9e985d2a6f462aca0660135e
ivanfsilva <ivanfs27@gmail.com>
2021-12-13 16:37:43 -0300

    should trigger emission when called

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -35,4 +35,14 @@ describe( LikeWidgetComponent.name, () => {
 
     expect( component.id ).toBe( someId );
   });
+
+  it(`#${ LikeWidgetComponent.prototype.like.name }
+    should trigger emission when called`, done => {
+      fixture.detectChanges();
+      component.liked.subscribe( () => {
+        expect(true).toBeTrue();
+        done();
+      });
+      component.like();
+  });
 });
