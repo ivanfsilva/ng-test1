3fd9d55b96e8a5d80ee205247e1276481d2c0c2f
ivanfsilva <ivanfs27@gmail.com>
2021-12-13 19:19:32 -0300

    bug fix

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -5,44 +5,41 @@ import { LikeWidgetModule } from './like-widget.module';
 
 describe( LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
+
   beforeEach( async () => {
     await TestBed.configureTestingModule({
       imports: [ LikeWidgetModule ]
     }).compileComponents();
 
     fixture = TestBed.createComponent( LikeWidgetComponent );
+    component = fixture.componentInstance;
   });
 
   it('should create component', () => {
-    const component = fixture.componentInstance;
-
     expect( component ).toBeTruthy();
   });
 
-  it('should auto generate ID when id input property is missing', () => {
-    const component = fixture.componentInstance;
+  it('Should auto-generate Id during ngOnInit when ( @Input id ) is not assigned', () => {
     fixture.detectChanges();
 
     expect( component.id ).toBeTruthy();
   });
 
-  it('should NOT generate ID when id input property is present', () => {
-    const component = fixture.componentInstance;
+  it('Should NOT auto-generate ID during ngOnInit when ( @Input id ) is assigned', () => {
     const someId = 'someId';
-
     component.id = someId;
+
     fixture.detectChanges();
 
     expect( component.id ).toBe( someId );
   });
 
   it(`#${ LikeWidgetComponent.prototype.like.name }
-    should trigger emission when called`, done => {
+    Should trigger ( @Output liked ) when called`, () => {
+      spyOn(component.liked, 'emit');
       fixture.detectChanges();
-      component.liked.subscribe( () => {
-        expect(true).toBeTrue();
-        done();
-      });
       component.like();
+      expect( component.liked.emit ).toHaveBeenCalled();
   });
 });
